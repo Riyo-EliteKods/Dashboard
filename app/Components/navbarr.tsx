51005922a27de48dad11c0a3c9c1f844d6c9c5f9
Riyo-EliteKods <riyo.elitekods@gmail.com>
2024-03-24 09:26:46 +0530

    24/03/2024

--- app/Components/navbarr.tsx
+++ app/Components/navbarr.tsx
@@ -1,9 +1,8 @@
 "use client";
 
+import Link from "next/link";
 import React, { useState } from "react";
 import { AiFillGold } from "react-icons/ai";
-
-
 
 const Navbarr = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,7 +18,9 @@ const Navbarr = () => {
             href="#"
             className="flex items-center space-x-1 rtl:space-x-reverse"
           >
-            <span className="font-extralight text-3xl bg-gradient-to-r from-orange-600 to-orange-400 p-1 ">Z</span>
+            <span className="font-extralight text-3xl bg-gradient-to-r from-orange-600 to-orange-400 p-1 ">
+              Z
+            </span>
             <span className="bg-gradient-to-r from-orange-600 to-orange-400 text-transparent bg-clip-text p-1 self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Zwappi
             </span>
@@ -27,26 +28,12 @@ const Navbarr = () => {
           <button
             onClick={toggleNavbar}
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 w-12 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-default"
             aria-expanded={isOpen ? "true" : "false"}
           >
             <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
+            <img src="../Images/Collapse-Icon.png" alt="" />
           </button>
           <div
             className={`w-full md:flex md:w-auto gap-240 ${
@@ -56,51 +43,59 @@ const Navbarr = () => {
           >
             <ul className="font-medium mb-2 flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <a
+                <Link
                   href="#"
                   className="block py-2 px-3 text-white bg-orange-600 rounded md:bg-transparent md:hover:text-orange-700 md:hover:underline md:p-0 dark:text-white "
                   aria-current="page"
                 >
                   Swap
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+                <Link
                   href="#"
                   className="block py-2 px-3 text-white rounded md:text-white  md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:hover:underline md:p-0 dark:text-white "
                 >
                   Bridge
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+                <Link
                   href="#"
                   className="block py-2 px-3 text-white  md:text-orange-700  md:hover:bg-transparent md:border-0  md:hover:border-b-2 md:hover:border-b-orange-800 md:p-0 dark:text-white"
                 >
                   Dashboard
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+                <Link
                   href="#"
                   className="block py-2 px-3 text-white rounded md:text-white  md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:hover:underline md:p-0 dark:text-white"
                 >
                   Analytics
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+                <Link
                   href="#"
                   className="block py-2 px-3 text-white rounded md:text-white  md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:hover:underline md:p-0 dark:text-white"
                 >
                   Contact
-                </a>
+                </Link>
               </li>
             </ul>
-            <div className="flex items-center gap-1 bg-gradient-to-r from-orange-600 to-orange-400 p-2 rounded-2xl text-white"><span><AiFillGold /></span> <span>degentrades.lens</span></div>
-
+            <div className="flex items-center gap-1 bg-gradient-to-r from-orange-600 to-orange-400 p-2 rounded-2xl text-white">
+              <span>
+                {" "}
+                <img
+                  src="../Images/Collapse-Icon.png"
+                  alt=""
+                  className="w-7 h-7"
+                />
+              </span>{" "}
+              <span>degentrades.lens</span>
+            </div>
           </div>
-         
         </div>
       </nav>
     </div>
